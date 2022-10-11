e3adffe919b78be21210eb617697e0de804f830c
elcode22 <elias.hammou@edu.edugrade.se>
2022-10-11 11:12:49 +0200

    Add project files.

---
+++ bilar/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace bilar
+{
+    interface IVehicle
+    {
+
+        int changeGear(int a);
+        int speedUp(int speed);
+        void applyBrakes();
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bicycel b = new bicycel();
+            bike B = new bike();
+
+            b.changeGear(0);
+            b.speedUp(10);
+            b.applyBrakes();
+
+            B.changeGear(0);
+            B.speedUp(10);
+            B.applyBrakes();
+
+            Console.WriteLine("Bicycle present state : " + b.changeGear(0) +"\n"+ b.speedUp(10));
+            Console.WriteLine("Bike present state : " + B.changeGear(0) +"\n"+ B.speedUp(12));
+        }
+    }
+}
---
+++ bilar/bicycel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace bilar
+{
+    class bicycel:IVehicle
+    {
+
+        public int changeGear(int a) 
+        {
+
+            Console.WriteLine("Gear changed");
+            a = 2;
+            return a;
+        
+        }
+        public int speedUp(int speed) 
+        {
+
+            speed += 1;
+            return speed;
+        
+        }
+        public void applyBrakes()
+        {
+            Console.WriteLine("Bilen har stannat");
+        
+        }
+
+    }
+}
